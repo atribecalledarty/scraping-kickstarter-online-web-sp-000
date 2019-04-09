0110fc6084aed2115f6fd72f5e5ecbf94e9882bc
Arthur Song <arth3rs0ng@gmail.com>
2019-04-09 06:40:48 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,14 @@
 # require libraries/modules here
+require "nokogiri"
 
 def create_project_hash
   # write your code here
+  html = File.ready("fixtures/kickstarter.html")
+  #reads kickstarter.html and sets equal to html string
+  kickstarter = Nokogiri::HTML(html)
+  #sets kickstarter equal to NodeSet converted from html string
+  
+  
+  
+  
 end
